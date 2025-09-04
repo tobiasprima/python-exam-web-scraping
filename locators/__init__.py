ff214ee11472159bc6409276445c8e368687c196
Tobias Prima <88434271+tobiasprima@users.noreply.github.com>
2025-09-04 22:57:10 +0800

    feat: search page locators

---
+++ locators/__init__.py
@@ -0,0 +1,1 @@
+from locators.search_page_locators import SearchPageLocators
---
+++ pages/__init__.py
@@ -0,0 +1,4 @@
+from pages.base_page import BasePage
+from pages.search_page import SearchPage
+from pages.result_page import ResultsPage
+from pages.details_page import DetailsPage
